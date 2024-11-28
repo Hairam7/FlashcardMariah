f4bb2185d16ef897f7188c0d78e72272f8ab0eaf
Maria <00001114223815sp@al.educacao.sp.gov.br>
2024-11-28 18:02:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Musica',
+    'Você conhece o Matuê?',
+    'Matheus Brasileiro Aguiar, mais conhecido pelo nome artístico Matuê, é um rapper, cantor, compositor e guitarrista brasileiro.'
+)
+
+criaCartao(
+    'Musica',
+    'Quem é natiruts?',
+    'Natiruts, inicialmente conhecida como Nativus, é uma banda de reggae brasileira formada em 1996 em Brasília. Chamada inicialmente Nativus, a banda foi rebatizada de Natiruts devido a um grupo catarinense de música nativista, Os Nativos, que entrou com um processo'
+)
+
+criaCartao(
+    'Musica',
+    'Voce conhece o Filipe Ret?',
+    'Filipe Cavaleiro de Macedo da Silva Faria, mais conhecido como Filipe Ret, é um rapper, cantor, compositor, comunicólogo, poeta e empresário brasileiro.'
+)
+
+criaCartao(
+    'Musica',
+    'Você conhece Mc Hariel?',
+    ' Hariel Denaro Ribeiro, mais conhecido como MC Hariel, é um cantor e compositor de funk paulista.'
+)
+criaCartao(
+    'Musica',
+    'Você conhece Kanye West?',
+    'nascido Kanye Omari West; Atlanta, (8 de junho de 1977), mais conhecido como Kanye West,é um rapper, cantor, compositor, produtor musical, político e designer de moda americano'
+)
+
+criaCartao(
+    'Musica',
+    'Você conhece Lady Gaga',
+    'Stefani Joanne Angelina Germanotta, mais conhecida pelo nome artístico Lady Gaga, é uma cantora, compositora, instrumentista, atriz, produtora musical e empresária estadunidense'
+)
